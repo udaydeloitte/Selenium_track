e80032766eb57e0f4507c7ec3c7e82aad0f3a6c1
udayprsingh <udayprsingh@deloitte.com>
2022-03-22 23:26:28 -0700

    home and logo path found

--- Mini_Assignment 1/Demosele.java
+++ Mini_Assignment 1/Demosele.java
@@ -4,23 +4,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Demosele {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\udayprsingh\\Downloads\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+        //Scanner sc=new Scanner(System.in);
+        //String choice=sc.next();
 
         driver.manage().window().maximize();
         driver.get("https://www.phptravels.net/hotels");
         driver.findElement(By.xpath("//img[contains(@alt,\"logo\")]"));
-        WebElement logo=driver.findElement(By.xpath("//img[contains(@alt,\"logo\")]"));
-        logo.click();
-        driver.close();
-        WebElement home=driver.findElement(By.xpath("//i[contains(@class,\"la la-bars\")]"));
-        home.click();
-        //first commit
+        WebElement phplogo = driver.findElement(By.xpath("//img[contains(@alt,\"logo\")]"));
+        phplogo.click();
         driver.quit();
 
-    }
+        driver.findElement(By.xpath("//i[contains(@class,\"la la-bars\")]"));
+
+        WebElement phphome = driver.findElement(By.xpath("//i[contains(@class,\"la la-bars\")]"));
+        phphome.click();
+        driver.quit();
+        
+
+
+
+
+            //driver.quit();
+        }
 }
