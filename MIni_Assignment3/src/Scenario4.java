52ba6b8f3f885df058099e036694c276352096f6
udayprsingh <udayprsingh@deloitte.com>
2022-03-23 22:57:18 -0700

    new york selected

--- MIni_Assignment3/src/Scenario4.java
+++ MIni_Assignment3/src/Scenario4.java
@@ -1,25 +1,27 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.idealized.Javascript;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class Scenario4 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\udayprsingh\\Downloads\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.goibibo.com/");
-        WebElement roundTrip = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/ul/li[2]/span[1]"));
-        roundTrip.click();
+        driver.get("https://www.goibibo.com/ ");
+        Actions action = new Actions(driver);
+
+        WebElement roundTripButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/ul/li[2]/span[1]"));
+        roundTripButton.click();
         WebElement From = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/p"));
         From.click();
         WebElement FromText = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/input"));
         FromText.click();
         FromText.sendKeys("New York (NYC)");
-        Select sel = new Select(FromText);
-
+        Thread.sleep(2000);
+        
+        WebElement selectFrom = driver.findElement(By.xpath("//*[@id=\"autoSuggest-list\"]/li[1]"));
+        action.click(selectFrom).build().perform();
 
 
     }
